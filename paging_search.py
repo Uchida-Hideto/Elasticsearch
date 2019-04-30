736344ef75dcf6fb4d098cadcf158f7d2a17e7d2
yuuri <houjie95@foxmail.com>
2019-04-30 16:10:50 +0800

    fix paging search

--- paging_search.py
+++ paging_search.py
@@ -5,7 +5,7 @@ LOG = logging.getLogger('Paging-Search')
 LOG.setLevel(level=logging.DEBUG)
 file_handler = logging.FileHandler('ElasticSearch.log')
 file_handler.setLevel(level=logging.DEBUG)
-log_format = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+log_format = logging.Formatter('%(asctime)s %(name)s %(process)d %(levelname)s %(message)s')
 file_handler.setFormatter(log_format)
 LOG.addHandler(file_handler)
 
@@ -22,19 +22,15 @@ class PagingSearch(object):
         except Exception as e:
             LOG.error('Can not connection Elasticsearch cause {}'.format(e))
 
-    def paging_search(self):
-        index = 'bank'
+    def paging_search(self, index,page_line):
         try:
             count = self.es.count(index=index)['count']
             LOG.info('Get the index {} count is {}'.format(index,count))
         except Exception as e:
             LOG.error('Can not get index {} count,cause {}'.format(index, e))
-        page_line = 5
-        page_remainder = count % page_line
-        if page_remainder == 0:
-            page = int(count/page_line)
-        else:
-            page = int(count/page_line+1)
+        page, page_reminder = divmod(count, page_line)
+        page = page if page_reminder == 0 else page+1
+        LOG.info('The Page count is {} and the page_remainder is {}'.format(page,page_reminder))
         for i in range(0, page):
             dsl = {
                 "query": {"match_all": {}},
@@ -51,4 +47,9 @@ class PagingSearch(object):
 if __name__ == '__main__':
     search = PagingSearch()
     search.ping()
-    search.paging_search()
+    index = input('print Elasticsearch index')
+    page_line = int(input('print the page line'))
+    search.paging_search(index='bank', page_line=page_line)
+
+
+
